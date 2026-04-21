1fb97654d9667202e52165d372da9576d7484203
prayag-01-ux <prayagdash797856@gmail.com>
2026-04-21 13:31:00 +0530

    Add UC3 class with pattern printing methods

---
+++ UC3.java
@@ -0,0 +1,30 @@
+public class UC3 {
+
+    public static void printLine1() {
+        System.out.println("*****  *****  *****  *****");
+    }
+
+    public static void printLine2() {
+        System.out.println("*   *  *   *  *   *  *");
+    }
+
+    public static void printLine3() {
+        System.out.println("*   *  *   *  *****  *****");
+    }
+
+    public static void printLine4() {
+        System.out.println("*   *  *   *  *      *");
+    }
+
+    public static void printLine5() {
+        System.out.println("*****  *****  *      *****");
+    }
+
+    public static void main(String[] args) {
+        printLine1();
+        printLine2();
+        printLine3();
+        printLine4();
+        printLine5();
+    }
+}
